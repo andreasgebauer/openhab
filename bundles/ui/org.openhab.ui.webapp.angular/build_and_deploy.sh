85cb33441a88e1556c3a889cd4a447ea7a463e87
agebauer <andreas.gebauer.berlin@gmail.com>
2015-12-23 16:09:04 +0100

    removed build and deploy script

--- bundles/ui/org.openhab.ui.webapp.angular/build_and_deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e
-
-mvn clean package -o
-
-scp /home/andreas/git/ag-openhab/bundles/ui/org.openhab.ui.webapp.angular/target/org.openhab.ui.webapp.angular-1.8.0-SNAPSHOT.jar pi@raspberrypi:/home/pi/
-ssh pi@raspberrypi "sudo mv org.openhab.ui.webapp.angular-1.8.0-SNAPSHOT.jar /usr/share/openhab/addons/"
-echo "Successfully copied to raspi"
